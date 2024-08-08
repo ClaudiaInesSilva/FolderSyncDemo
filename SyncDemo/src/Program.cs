a554ed404ea2abe1c9e09415a69720d6a351a2d7
Claudia Silva [1221825] <1221825@isep.ipp.pt>
2024-08-08 23:37:07 +0100

    Add project files.

---
+++ SyncDemo/src/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+internal class Program
+{
+
+    // Accept four arguments: 'source' path, 'replica' path, 'log' file path and 'interval'(seconds)
+    // Timer for synchronization (receives 'interval') 
+    // Synchronization - one way 'source' to 'replica'
+    // Logging - 
+
+    private static void Main(string[] args)
+    {
+
+    }
+
+
+}
