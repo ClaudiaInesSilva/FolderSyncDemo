88f25ca8fa71790c2017c7d62d5869f081d0f320
Claudia Silva [1221825] <1221825@isep.ipp.pt>
2024-08-10 02:23:58 +0100

    Added Main code and Timer class

--- SyncDemo/src/Program.cs
+++ SyncDemo/src/Program.cs
@@ -8,7 +8,35 @@ namespace SyncDemo
 
         private static void Main(string[] args)
         {
+            try
+            {
+                var commandLine = new CommandLine(args);
+                var logger = new Logger(commandLine.LogFilePath);
 
+                var replicaFolderName = $"{Path.GetFileName(commandLine.SourcePath)}_replica";
+                var replicaPath = Path.Combine(commandLine.ReplicaPath, replicaFolderName);
+
+                if (!Directory.Exists(replicaPath))
+                {
+                    Directory.CreateDirectory(replicaPath);
+                    var replicaCreatedMessage = "$Created replica folder at {replicaPath}";
+                    Console.WriteLine(replicaCreatedMessage);
+                    logger.Log(replicaCreatedMessage);
+                }
+
+
+                var manager = new Manager(logger);
+                var synchronizer = new FolderSynchronization(commandLine.SourcePath, commandLine.ReplicaPath, manager, logger);
+                var syncTimer = new SyncTimer(synchronizer, commandLine.SyncInterval);
+
+                var message = "Synchronization has started. Press any key to exit.";
+                Console.WriteLine(message);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Application error: {e.Message}");
+            }
 
         }
 
---
+++ SyncDemo/src/SyncTimer.cs
@@ -0,0 +1,21 @@
+﻿namespace SyncDemo.src
+{
+    internal class SyncTimer
+    {
+        private readonly FolderSynchronization _folderSynchronization;
+        private readonly Timer _timer;
+
+        public SyncTimer(FolderSynchronization folderSynchronization, int syncInterval)
+        {
+            _folderSynchronization = folderSynchronization;
+            _timer = new Timer(CallSync, null, TimeSpan.Zero, TimeSpan.FromSeconds(syncInterval));
+            Console.WriteLine($"Syncronization timer started with an interval of {syncInterval}");
+        }
+
+        private void CallSync(object? state)
+        {
+            _folderSynchronization.SynchronizeFolders();
+        }
+
+    }
+}
