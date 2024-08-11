ed0e8356c2cba339f0cce61adf49697b0b6885a4
Claudia Silva [1221825] <1221825@isep.ipp.pt>
2024-08-11 04:45:24 +0100

    Add StartApp to handle app setup and execution logic. Refactored Program

--- SyncDemo/src/Program.cs
+++ SyncDemo/src/Program.cs
@@ -1,31 +1,10 @@
-﻿using SyncDemo.src;
-
-namespace SyncDemo
+﻿namespace SyncDemo
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            try
-            {
-                var commandLine = new CommandLine(args);
-                var logger = new Logger(commandLine.LogFilePath);
-
-                var replicaFolderName = $"{Path.GetFileName(commandLine.SourcePath)}_replica";
-                var replicaPath = Path.Combine(commandLine.ReplicaPath, replicaFolderName);
-
-                var manager = new Manager(logger);
-                var synchronizer = new FolderSynchronization(commandLine.SourcePath, replicaPath, manager, logger);
-                var syncTimer = new SyncTimer(synchronizer, commandLine.SyncInterval);
-
-                var message = "INFO - Synchronization has started. Press CTRL+C to exit.";
-                Console.WriteLine(message);
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"Application Error: {e.Message}");
-            }
+            App.StartApp(args);
         }
     }
 }
---
+++ SyncDemo/src/StartApp.cs
@@ -0,0 +1,31 @@
+﻿using SyncDemo.src;
+
+namespace SyncDemo
+{
+    internal static class App
+    {
+        public static void StartApp(string[] args)
+        {
+            try
+            {
+                var commandLine = new CommandLine(args);
+                var logger = new Logger(commandLine.LogFilePath);
+
+                var replicaFolderName = $"{Path.GetFileName(commandLine.SourcePath)}_replica";
+                var replicaPath = Path.Combine(commandLine.ReplicaPath, replicaFolderName);
+
+                var manager = new Manager(logger);
+                var synchronizer = new FolderSynchronization(commandLine.SourcePath, replicaPath, manager, logger);
+                var syncTimer = new SyncTimer(synchronizer, commandLine.SyncInterval);
+
+                var message = "INFO - Synchronization has started. Press CTRL+C to exit.";
+                Console.WriteLine(message);
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Application Error: {e.Message}");
+            }
+        }
+    }
+}
